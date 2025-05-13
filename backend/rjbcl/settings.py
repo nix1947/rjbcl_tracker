53d77d32e2b99841b2cdb0bab18b8a7179c73466
Manoj Gautam <manojit.gautam@gmail.com>
2025-05-13 23:09:35 +0545

    updated urls

--- backend/rjbcl/settings.py
+++ backend/rjbcl/settings.py
@@ -159,7 +159,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
--- backend/rjbcl/urls.py
+++ backend/rjbcl/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.urls import path, re_path, include
+from django.conf.urls.static import static
 from rest_framework import permissions
+from django.conf import  settings
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from statement_tracker.views import api_index, dashboard
@@ -51,6 +53,13 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
 
 
 
