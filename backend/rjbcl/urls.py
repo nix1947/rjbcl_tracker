39d3fffd490c505058830b04e65569532b81cbb9
Manoj Gautam <manoj.gautam@rjbcl.gov.np>
2026-01-25 12:18:07 +0545

    API endpoint removed

--- backend/rjbcl/urls.py
+++ backend/rjbcl/urls.py
@@ -25,19 +25,6 @@ from drf_yasg import openapi
 from statement_tracker.views import api_index, dashboard
 from user_request_app import  views as user_request_app_view
 
-# Schema view for swagger
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Your API Title",
-      default_version='v1',
-      description="API documentation with Swagger",
-      terms_of_service="https://www.example.com/terms/",
-      contact=openapi.Contact(email="you@example.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 
 urlpatterns = [
@@ -47,16 +34,13 @@ urlpatterns = [
 
 
     # kyc app
-    path('kyc/', include('kyc.urls')),
+    # path('kyc/', include('kyc.urls')),
 
 
  # Sample dashboard
      path('dashboard/', dashboard, name='dashboard'),
 
 # URLS for swagger
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 
     ## Admin related urls
--- backend/statement_tracker/urls.py
+++ backend/statement_tracker/urls.py
@@ -9,13 +9,13 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/admin/', permanent=True)),
+    path('', RedirectView.as_view(url='/admin/', permanent=True))
 
-    # path('', api_index, name='api_index'),  # ✅ Keep at root if needed
-    path('api/', include(router.urls)),         # ✅ Make sure router is included right after
-    path('api/auth/password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
-    path('api/auth/password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('api/auth/change-password/', PasswordChangeView.as_view(), name='change_password'),
-    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # # path('', api_index, name='api_index'),  # ✅ Keep at root if needed
+    # path('api/', include(router.urls)),         # ✅ Make sure router is included right after
+    # path('api/auth/password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    # path('api/auth/password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('api/auth/change-password/', PasswordChangeView.as_view(), name='change_password'),
+    # path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
